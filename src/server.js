b1c42edf51cbaa3862edfd460bd629251ccf01f5
david <david@appsaloon.be>
2018-01-28 19:13:19 +0100

    concurrent external API calls

--- src/server.js
+++ src/server.js
@@ -20,11 +20,20 @@ const externals = {
 app.post('/price/:pair1/:pair2', async function (req, res) {
   const p = req.params
 
-  const poloniex = await externals.poloniex.call(p.pair1, p.pair2)
-  const binance = await externals.binance.call(p.pair1, p.pair2)
-  const kucoin = await externals.kucoin.call(p.pair1, p.pair2)
-  const kraken = await externals.kraken.call(p.pair1, p.pair2)
-  const gdax = await externals.gdax.call(p.pair1, p.pair2)
+  const [
+    poloniex,
+    binance,
+    kucoin,
+    kraken,
+    gdax
+  ] = await Promise.all([
+    externals.poloniex.call(p.pair1, p.pair2),
+    externals.binance.call(p.pair1, p.pair2),
+    externals.kucoin.call(p.pair1, p.pair2),
+    externals.kraken.call(p.pair1, p.pair2),
+    externals.gdax.call(p.pair1, p.pair2)
+  ])
+
   const result = {
     poloniex,
     binance,
