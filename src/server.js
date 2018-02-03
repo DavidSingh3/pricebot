5641490aecd64aafdbd77c740e3489aa10d6c983
david <david@appsaloon.be>
2018-02-03 19:57:38 +0100

    rounding of prices

--- src/server.js
+++ src/server.js
@@ -26,67 +26,77 @@ const externals = {
 
 // routes will go here
 app.post('/price/:pair1/:pair2', async function (req, res) {
-  const p1 = req.params.pair1
-  const p2 = req.params.pair2
+  try {
+    const p1 = req.params.pair1
+    const p2 = req.params.pair2
 
-  const [
-    poloniex,
-    binance,
-    kucoin,
-    kraken,
-    gdax,
-    bitfinex,
-    shapeshift,
-    quadrigacx,
-    lakebtc,
-    hitbtc,
-    bisq,
-    bitz,
-    localbitcoins
-  ] = await Promise.all([
-    externals.poloniex.call(p1, p2),
-    externals.binance.call(p1, p2),
-    externals.kucoin.call(p1, p2),
-    externals.kraken.call(p1, p2),
-    externals.gdax.call(p1, p2),
-    externals.bitfinex.call(p1, p2),
-    externals.shapeshift.call(p1, p2),
-    externals.quadrigacx.call(p1, p2),
-    externals.lakebtc.call(p1, p2),
-    externals.hitbtc.call(p1, p2),
-    externals.bisq.call(p1, p2),
-    externals.bitz.call(p1, p2),
-    externals.localbitcoins.call(p1, p2)
-  ])
+    const [
+      poloniex,
+      binance,
+      kucoin,
+      kraken,
+      gdax,
+      bitfinex,
+      shapeshift,
+      quadrigacx,
+      lakebtc,
+      hitbtc,
+      bisq,
+      bitz,
+      localbitcoins
+    ] = await Promise.all([
+      externals.poloniex.call(p1, p2),
+      externals.binance.call(p1, p2),
+      externals.kucoin.call(p1, p2),
+      externals.kraken.call(p1, p2),
+      externals.gdax.call(p1, p2),
+      externals.bitfinex.call(p1, p2),
+      externals.shapeshift.call(p1, p2),
+      externals.quadrigacx.call(p1, p2),
+      externals.lakebtc.call(p1, p2),
+      externals.hitbtc.call(p1, p2),
+      externals.bisq.call(p1, p2),
+      externals.bitz.call(p1, p2),
+      externals.localbitcoins.call(p1, p2)
+    ])
 
-  const result = [
-    poloniex,
-    binance,
-    kucoin,
-    kraken,
-    gdax,
-    bitfinex,
-    shapeshift,
-    quadrigacx,
-    lakebtc,
-    hitbtc,
-    bisq,
-    bitz,
-    localbitcoins
-  ].reduce((accumulator, current) => {
-    if(current)
-      accumulator.push(current)
-    return accumulator
-  }, []).sort((a, b) =>{
-    if(parseFloat(a.v) > parseFloat(b.v))
-      return -1
-    if(parseFloat(a.v) < parseFloat(b.v))
-      return 1
-    return 0
-  })
+    const result = [
+      poloniex,
+      binance,
+      kucoin,
+      kraken,
+      gdax,
+      bitfinex,
+      shapeshift,
+      quadrigacx,
+      lakebtc,
+      hitbtc,
+      bisq,
+      bitz,
+      localbitcoins
+    ].reduce((accumulator, current) => {
+      if (current) {
+        while (current.p[current.p.length - 1] === '0' || current.p[current.p.length - 1] === '.') {
+          current.p = [...current.p]
+          current.p.pop()
+          current.p = current.p.join('')
+        }
+        accumulator.push(current)
+      }
+      return accumulator
+    }, []).sort((a, b) => {
+      if (parseFloat(a.v) > parseFloat(b.v))
+        return -1
+      if (parseFloat(a.v) < parseFloat(b.v))
+        return 1
+      return 0
+    })
 
-  console.log(result)
-  res.json({})
+    res.json(result)
+  } catch (e) {
+    console.log(e)
+    res.json([])
+  }
 })
 
 // start the server
